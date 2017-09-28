fd31610567a795bb99e2efcafa6c3b2ad8610030
adambreznicky <smudgeservices@gmail.com>
2017-09-28 11:03:47 -0500

    handle no historical aerials in database. reorganized views

--- lakegallery/map/views.py
+++ lakegallery/map/views.py
@@ -6,17 +6,28 @@ from django.shortcuts import render_to_response
 from .models import MajorReservoirs, RWPAs, HistoricalAerialLinks
 
 
-def index(request, letter=""):
+### utility functions
+def get_region_header_list():
     l = RWPAs.objects.values_list('reg_name', 'letter')
     labels = [{'name': r[0], 'letter': r[1]} for r in l]
     labels.sort(key=lambda x: x['name'])
-    # print(labels)
+    return labels
+
+
+def get_lake_header_list():
     r = MajorReservoirs.objects.values_list('res_lbl', 'region')
     res = [{'name': n[0], 'region': n[1]} for n in r]
     res.sort(key=lambda x: x['name'])
-    # print(res)
+    return res
+
+
+### views/templates & redirects
+def index(request, letter=""):
+    labels = get_region_header_list()
+    res = get_lake_header_list()
     context = {'header_regions': labels, 'header_lakes': res,
                'layers': layers, 'region': letter}
+
     return render(request, 'map/index.html', context)
 
 
@@ -26,14 +37,21 @@ def redirect_region(request, letter):
 
 
 def story(request, letter, lake):
-    r = MajorReservoirs.objects.get(res_lbl=lake)
-    l = HistoricalAerialLinks.objects.filter(lake=r)
-    links = [obj.as_dict() for obj in l]
+    labels = get_region_header_list()
+    res = get_lake_header_list()
+
+    m = MajorReservoirs.objects.get(res_lbl=lake)
+    n = HistoricalAerialLinks.objects.filter(lake=m)
+    links = [obj.as_dict() for obj in n]
     links.sort(key=lambda x: x['year'])
-    context = {'layer': layers['reservoirs'], 'lake': lake, 'links': links}
+
+    context = {'header_regions': labels, 'header_lakes': res,
+               'layer': layers['reservoirs'], 'lake': lake, 'links': links}
+
     return render(request, 'map/story.html', context)
 
 
+### error code handling
 def bad_request(request):
     context = {'code': 400, 'text': 'Bad Request'}
     response = render_to_response('map/error.html', context)
