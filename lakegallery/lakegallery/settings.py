8ee98bbd8c69de7ac68335e8485faa93374218f8
adambreznicky <smudgeservices@gmail.com>
2017-11-01 14:06:08 -0500

    api permissions and index/region map css

--- lakegallery/lakegallery/settings.py
+++ lakegallery/lakegallery/settings.py
@@ -166,8 +166,8 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-        'rest_framework.permissions.IsAdminUser',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        # 'rest_framework.permissions.IsAdminUser',
     ],
     'PAGE_SIZE': 10
 }
