2b4be4486495c8b454cdbd3d794bd2b958af53f6
adambreznicky <smudgeservices@gmail.com>
2017-11-02 13:55:13 -0500

    adjusted comments

--- lakegallery/api/views.py
+++ lakegallery/api/views.py
@@ -11,7 +11,7 @@ from map.models import MajorReservoirs, RWPAs
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows users to be viewed
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
@@ -19,7 +19,7 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allows groups to be viewed or edited.
+    API endpoint that allows groups to be viewed
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
