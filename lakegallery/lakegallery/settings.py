763fd3dde15ae20fa870021eb5a6c6e2f5969ae5
adambreznicky <smudgeservices@gmail.com>
2017-11-09 10:41:16 -0600

    fix long map on firefox/safari

--- lakegallery/lakegallery/settings.py
+++ lakegallery/lakegallery/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '0.0.0.0', '.tnris.org', '.tnris.org.']
 
