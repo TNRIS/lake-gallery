071fbeabdc52f9db2f7e0eb58e9786a69e2bc74d
adambreznicky <smudgeservices@gmail.com>
2017-11-10 15:12:08 -0600

    add admin template recognition

--- lakegallery/lakegallery/settings.py
+++ lakegallery/lakegallery/settings.py
@@ -65,7 +65,8 @@ ROOT_URLCONF = 'lakegallery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'api', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'api', 'templates'),
+                 os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
