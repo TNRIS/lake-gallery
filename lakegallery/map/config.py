4856a3607d4009fedd560c1590ec2f6309e16457
John Haney <geojwh@gmail.com>
2019-05-09 15:48:11 -0500

    index home page has pts; in progress zoom level switch to polygons
    related to issues #57, #40, #38, #35

--- lakegallery/map/config.py
+++ lakegallery/map/config.py
@@ -1,26 +1,26 @@
 layers = {
-    # "rwpas": {
-    #     "table_name": "rwpas",
-    #     "label_field": "reg_name",
-    #     "carto_css": '#layer {polygon-fill: ramp([letter], (#5F4690, #1D6996, #38A6A5, #0F8554, #73AF48, #EDAD08, #E17C05, #f7604a, #94346E, #6F4070, #94ec67, #842c00, #4cf7ec, #8c8ec4, #ffc0c0, #ffed4a, #666666), ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"), "=");}#layer::outline {line-width: 1;line-color: #FFF;line-opacity: 0.5;}',
-    #     "carto_lbl": '#layer::labels {text-name: [reg_name];text-face-name: "DejaVu Sans Book";text-size: 12;text-fill: #FFFFFF;text-label-position-tolerance: 0;text-halo-radius: 1;text-halo-fill: #6f808d;text-dy: 0;text-allow-overlap: false;text-placement: point;text-placement-type: dummy;}',
-    #     "interactivity": ["letter", "reg_name"]
-    # },
+    "rwpas": {
+        "table_name": "rwpas",
+        "label_field": "reg_name",
+        "carto_css": '#layer {polygon-fill: ramp([letter], (#5F4690, #1D6996, #38A6A5, #0F8554, #73AF48, #EDAD08, #E17C05, #f7604a, #94346E, #6F4070, #94ec67, #842c00, #4cf7ec, #8c8ec4, #ffc0c0, #ffed4a, #666666), ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"), "=");}#layer::outline {line-width: 1;line-color: #FFF;line-opacity: 0.5;}',
+        "carto_lbl": '#layer::labels {text-name: [reg_name];text-face-name: "DejaVu Sans Book";text-size: 12;text-fill: #FFFFFF;text-label-position-tolerance: 0;text-halo-radius: 1;text-halo-fill: #6f808d;text-dy: 0;text-allow-overlap: false;text-placement: point;text-placement-type: dummy;}',
+        "interactivity": ["letter", "reg_name"]
+    },
+    "reservoirs_pt": {
+        "table_name": "reservoirs_pt",
+        "label_field": "res_lbl",
+        "carto_css": '#layer [story="enabled"] {marker-width: 10; marker-fill: #1556d7; marker-fill-opacity: 0.9; marker-allow-overlap: true; marker-line-width: 1; marker-line-color: #FFFFFF; marker-line-opacity: 1;}',
+        # "carto_lbl": '#layer::labels {[story="enabled"]{text-name: [res_lbl]; text-face-name: "DejaVu Sans Book";text-size: 12; text-fill: #FFFFFF; text-label-position-tolerance: 0; text-halo-radius: 1; text-halo-fill: #6f808d;text-dy: -10;text-allow-overlap: true;text-placement: point;text-placement-type: dummy;}}',
+        "interactivity": ["res_lbl"],
+        "carto_story_css": '#layer{[story="enabled"]{polygon-fill: #5491f2; polygon-opacity: 0.25;}[story="disabled"]{polygon-fill: #acacac;polygon-opacity: 1;}}#layer::outline{[story="enabled"]{line-width: 2;line-color: #5491f2;line-opacity: 0.4;}[story="disabled"]{line-width: 2;line-color: #515151;line-opacity: 0.2;}}'
+    },
     "reservoirs_poly": {
         "table_name": "reservoirs",
         "label_field": "res_lbl",
-        "carto_css": '#layer{polygon-fill: ramp([story], (#005dff, #acacac), ("enabled", "disabled"), "="); polygon-opacity: 1;}#layer::outline{[story="enabled"]{line-width: 7.5; line-color: #5491f2; line-opacity: 0.5;}[story="disabled"]{line-width: 6;line-color: #515151;line-opacity: 0.2;}}#layer::outline {[story="enabled"]{line-width: 15;line-color: #c4d5f2;line-opacity: 0.5;}}',
+        "carto_css": '#layer [story="enabled"] {polygon-fill: ramp([story], (#005dff, #acacac), ("enabled", "disabled"), "="); polygon-opacity: 1;}#layer::outline{[story="enabled"]{line-width: 7.5; line-color: #5491f2; line-opacity: 0.5;}}#layer::outline {[story="enabled"]{line-width: 15;line-color: #c4d5f2;line-opacity: 0.5;}}',
         "carto_lbl": '#layer::labels {[story="enabled"]{text-name: [res_lbl]; text-face-name: "DejaVu Sans Book"; text-size: 12; text-fill: #FFFFFF; text-label-position-tolerance: 0; text-halo-radius: 1; text-halo-fill: #6f808d;text-dy: -10;text-allow-overlap: true;text-placement: point;text-placement-type: dummy;}}',
-        "interactivity": ["region", "res_lbl"],
-        "carto_story_css": '#layer{[story="enabled"]{polygon-fill: #5491f2; polygon-opacity: 0.25;}[story="disabled"]{polygon-fill: #acacac; polygon-opacity: 1;}}#layer::outline{[story="enabled"]{line-width: 2;line-color: #5491f2;line-opacity: 0.4;}[story="disabled"]{line-width: 2;line-color: #515151;line-opacity: 0.2;}}'
-    },
-    "reservoirs_point": {
-        "table_name": "reservoirs",
-        "label_field": "res_lbl",
-        "carto_css": '#layer{polygon-fill: ramp([story], (#005dff, #acacac), ("enabled", "disabled"), "="); polygon-opacity: 1;}#layer::outline{[story="enabled"]{line-width: 7.5; line-color: #5491f2; line-opacity: 0.5;}[story="disabled"]{line-width: 6;line-color: #515151;line-opacity: 0.2;}}#layer::outline {[story="enabled"]{line-width: 15;line-color: #c4d5f2;line-opacity: 0.5;}}',
-        "carto_lbl": '#layer::labels {[story="enabled"]{text-name: [res_lbl]; text-face-name: "DejaVu Sans Book";text-size: 12; text-fill: #FFFFFF; text-label-position-tolerance: 0; text-halo-radius: 1; text-halo-fill: #6f808d;text-dy: -10;text-allow-overlap: true;text-placement: point;text-placement-type: dummy;}}',
-        "interactivity": ["res_lbl"],
-        "carto_story_css": '#layer{[story="enabled"]{polygon-fill: #5491f2; polygon-opacity: 0.25;}[story="disabled"]{polygon-fill: #acacac;polygon-opacity: 1;}}#layer::outline{[story="enabled"]{line-width: 2;line-color: #5491f2;line-opacity: 0.4;}[story="disabled"]{line-width: 2;line-color: #515151;line-opacity: 0.2;}}'
+        "interactivity": ["region", "res_lbl"]
+        # "carto_story_css": '#layer{[story="enabled"]{polygon-fill: #5491f2; polygon-opacity: 0.25;}[story="disabled"]{polygon-fill: #acacac; polygon-opacity: 1;}}#layer::outline{[story="enabled"]{line-width: 2;line-color: #5491f2;line-opacity: 0.4;}[story="disabled"]{line-width: 2;line-color: #515151;line-opacity: 0.2;}}'
     }
 }
 # overlays dict key name must equal key[toc_label] which must also equal
