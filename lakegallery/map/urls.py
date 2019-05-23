ee7174112986b1ce3f45aaea8d48782852f1a1a8
John Haney <geojwh@gmail.com>
2019-05-23 14:11:25 -0500

    resolved url pattern regex issue with about page

--- lakegallery/map/urls.py
+++ lakegallery/map/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 app_name = 'map'
 urlpatterns = [
+    url(r'^about/$', views.about, name='about'),
     url(r'^([\w|\W]+)$', views.story, name='story'),
     url(r'^([\w|\W]+)$', views.redirect_story),
-    url(r'^about/$', views.about, name='about'),
+    # 
     url(r'^$', views.index, name='index')
 ]
