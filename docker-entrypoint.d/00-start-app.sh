cb3e83b1994f3108863594c6d00419b5e723d2f1
Adam Breznicky <smudgeservices@gmail.com>
2019-10-17 09:57:13 -0500

    added recaptcha to contact. handle client notice if response shows submission failed.

--- docker-entrypoint.d/00-start-app.sh
+++ docker-entrypoint.d/00-start-app.sh
@@ -11,6 +11,9 @@ export DB_PORT=`/envs/lake-gallery/bin/aws ssm get-parameters --name /apps/lake_
 export AWS_STORAGE_BUCKET_NAME=`/envs/lake-gallery/bin/aws ssm get-parameters --name /apps/lake_gallery/aws_storage_bucket_name --region us-east-1 --output text | awk '{print $6}'`
 export AWS_S3_REGION_NAME=`/envs/lake-gallery/bin/aws ssm get-parameters --name /apps/lake_gallery/aws_s3_region_name --with-decryption --region us-east-1 --output text | awk '{print $6}'`
 
+export CONTACT_SUBMIT_URL=`/envs/lake-gallery/bin/aws ssm get-parameters --name /apps/lake_gallery/contact_submit_url --region us-east-1 --output text | awk '{print $6}'`
+export RECAPTCHA_SITE_KEY=`/envs/lake-gallery/bin/aws ssm get-parameters --name /apps/lake_gallery/recaptcha_site_key --region us-east-1 --output text | awk '{print $6}'`
+
 export LAKE_GALLERY_MODE=`/envs/lake-gallery/bin/aws ssm get-parameters --name /apps/lake_gallery/lake_gallery_mode --region us-east-1 --output text | awk '{print $6}'`
 
 eval /lakegallery/manage.py collectstatic --noinput "$@"
